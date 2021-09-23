8bcd87cdc2373ac76b6925403bb5af459789558f
priegels <88594441+priegels@users.noreply.github.com>
2021-09-23 14:23:27 +0200

    Revert "installed modules for API"
    This reverts commit 4683c8daffe0c8b0a3320d8b00b44091a75066f1.

---
+++ server.js
@@ -0,0 +1,38 @@
+//import modules
+const http = require('http'),
+    fs = require('fs'),
+    url = require('url');
+
+http.createServer((request, response) => {
+    let addr = request.url,
+        q = url.parse(addr, true),
+        filePath = '';
+
+    //logs request URL and timestamp to log.txt
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+    });
+
+    //if URL contains 'documentation', returns documentation.html to user; otherwise index.html 
+    if (q.pathname.includes('documentation')) {
+        filePath = (__dirname + '/documentation.html');
+    } else {
+        filePath = 'index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        response.writeHead(200, {'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+    });
+}).listen(8080);
+
+console.log('My test server is running Port 8080.');
