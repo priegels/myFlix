ad384d0ebf875cc1b97f79e7438aba280cc56175
priegels <88594441+priegels@users.noreply.github.com>
2021-09-25 13:40:24 +0200

    added error handling middleware

--- index.js
+++ index.js
@@ -114,6 +114,13 @@ app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
+//error handling middleware
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 // listen for requests
 
 app.listen(8080, () => {
