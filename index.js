045f5d5e4f66d8934a9821051beca15e1ef13660
priegels <philippriegels@gmail.com>
2022-10-11 19:24:11 +0200

    added netlify to CORS

--- index.js
+++ index.js
@@ -35,7 +35,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // enabling CORS
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:1234', 'https://k-flix.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // if a specific origin isn't found on the list of allowed origins 
+      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //calling passport and authorization
 let auth = require('./auth')(app);
