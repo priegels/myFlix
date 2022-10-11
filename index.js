0144f9777cf5f0e26319f1ddb8f6328edd4b83f5
priegels <88594441+priegels@users.noreply.github.com>
2022-10-11 21:26:09 +0200

    [fix] removed cors allowedOrigins

--- index.js
+++ index.js
@@ -35,18 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // enabling CORS
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'https://k-flix.netlify.app'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // if a specific origin isn't found on the list of allowed origins 
-      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 //calling passport and authorization
 let auth = require('./auth')(app);
